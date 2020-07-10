814cee763e9e4762eb6ed791de3915dd62513a35
khantmawhein <kokhant228abc@gmail.com>
2020-07-11 00:39:28 +0630

    commit for movies

---
+++ src/components/Search.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Search({handleInput, search }) {
+  return (
+    <section className="searchbox-wrap">
+      <input
+        type="text"
+        className="searchbox"
+        placeholder="Search for a Movie..."
+        onChange={handleInput}
+        onKeyPress={search}
+      />
+    </section>
+  );
+}
+
+export default Search;
