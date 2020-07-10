a5fcf0791ede525ac6ed73d3fa202cb2fba8a232
khantmawhein <kokhant228abc@gmail.com>
2020-07-11 01:17:46 +0630

    Updates

---
+++ precache-manifest.e3257041501cef423fcf64eeee71ef37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c824273e7ef1ea124b26abed77d95392",
+    "url": "/my-movies-app/index.html"
+  },
+  {
+    "revision": "0e3bcaa9afbab79bbf13",
+    "url": "/my-movies-app/static/css/main.585915a3.chunk.css"
+  },
+  {
+    "revision": "07deda1ef15da77a90a8",
+    "url": "/my-movies-app/static/js/2.77426835.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-movies-app/static/js/2.77426835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3bcaa9afbab79bbf13",
+    "url": "/my-movies-app/static/js/main.1797b1f0.chunk.js"
+  },
+  {
+    "revision": "40c6c7e54c052d3f7c6c",
+    "url": "/my-movies-app/static/js/runtime-main.4ff9266e.js"
+  }
+]);
